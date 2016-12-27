69c642b2507bf980cbd3027b3ba12cbcac79fbe8
andythenorth <mail@andythenorth.co.uk>
2016-12-27 09:38:20 +0000

    Codechange: add timing info to python scripts in compile, and stub in an empty render_graphics.py

---
+++ src/render_graphics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+  This file is part of Unsinkable Sam Newgrf for OpenTTD.
+  Unsinkable Sam is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+  Unsinkable Sam is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Road Hog. If not, see <http://www.gnu.org/licenses/>.
+"""
+print("[RENDER GRAPHICS] render_graphics.py")
+
+def main():
+    # just a stub for now
+    pass
+
+if __name__ == '__main__':
+    main()
