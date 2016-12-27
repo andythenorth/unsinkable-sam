7d2f7a26e57935252cbc8e3b05bd7515569f97b1
andythenorth <mail@andythenorth.co.uk>
2016-12-27 10:48:26 +0000

    Codechange: use same render_nml.py as Road Hog, configured accordingly

--- src/render_nml.py
+++ src/render_nml.py
@@ -1,40 +1,66 @@
+#!/usr/bin/env python
+
 print("[RENDER NML] render_nml.py")
 
-import os.path
-currentdir = os.curdir
+import codecs # used for writing files - more unicode friendly than standard open() module
 
 import sys
-sys.path.append(os.path.join('src')) # add to the module search path
-
-import codecs # used for writing files - more unicode friendly than standard open() module
+import os
+currentdir = os.curdir
+from multiprocessing import Pool
 
 import unsinkable_sam
 import utils
 import global_constants
-
 from rosters import registered_rosters
 
 # get args passed by makefile
 repo_vars = utils.get_repo_vars(sys)
+num_pool_workers = repo_vars.get('num_pool_workers', 0) # default to no mp, makes debugging easier (mp fails to pickle errors correctly)
+if num_pool_workers == 0:
+    use_multiprocessing = False
+else:
+    use_multiprocessing = True
 
 from chameleon import PageTemplateLoader # chameleon used in most template cases
 # setup the places we look for templates
 templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'templates'))
 
+generated_nml_path = os.path.join(unsinkable_sam.generated_files_path, 'nml')
+if not os.path.exists(generated_nml_path):
+    os.mkdir(generated_nml_path)
+
+def render_consist_nml(consist):
+    consist_nml = codecs.open(os.path.join('generated', 'nml', consist.id + '.nml'),'w','utf8')
+    consist_nml.write(utils.unescape_chameleon_output(consist.render()))
+    consist_nml.close()
 
 def main():
-    ships = unsinkable_sam.get_ships_in_buy_menu_order()
+    # 'consists' even though they are ships, it's just more consistent with other grfs, makes maintaining these files simpler
+    consists = unsinkable_sam.get_ships_in_buy_menu_order()
 
     grf_nml = codecs.open(os.path.join('unsinkable-sam.nml'),'w','utf8')
-    header_items = ['header', 'cargo_table', 'disable_default_ships']
+    header_items = ['header', 'cargo_table', 'disable_default_vehicles']
     for header_item in header_items:
         template = templates[header_item + '.pynml']
-        grf_nml.write(utils.unescape_chameleon_output(template(ships=ships, global_constants=global_constants,
-                                                        registered_rosters=registered_rosters, utils=utils,
-                                                        sys=sys, repo_vars=repo_vars)))
+        grf_nml.write(utils.unescape_chameleon_output(template(consists=consists, global_constants=global_constants,
+                                                      registered_rosters=registered_rosters,
+                                                      utils=utils, sys=sys, repo_vars=repo_vars)))
 
-    for ship in set(ships):
-        grf_nml.write(utils.unescape_chameleon_output(ship.render()))
+    if use_multiprocessing == False:
+        utils.echo_message('Multiprocessing disabled: (pw=0)')
+        for consist in consists:
+            render_consist_nml(consist)
+    else:
+        # multiprocessing is not always a win for rendering chameleon templates, the overhead can increase render time substantially
+        # worth testing occasionally for empirical results
+        pool = Pool(processes=num_pool_workers)
+        pool.map(render_consist_nml, consists)
+
+    for consist in consists:
+        # makefile passes 'roster' arg, but there is no python compile support for compiling a single roster yet
+        consist_nml = codecs.open(os.path.join('generated', 'nml', consist.id + '.nml'),'r','utf8').read()
+        grf_nml.write(consist_nml)
 
     grf_nml.close()
 
