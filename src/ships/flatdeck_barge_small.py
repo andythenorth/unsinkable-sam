9a2fe235ca5385b4f6ff95ef1dbed554bb5b2562
andythenorth <mail@andythenorth.co.uk>
2017-01-03 21:41:22 +0000

    Change: more ship style development; also add small flatdeck barge

---
+++ src/ships/flatdeck_barge_small.py
@@ -0,0 +1,19 @@
+import global_constants
+from ship import FlatDeckBarge
+
+ship = FlatDeckBarge(id = 'flatdeck_barge_small',
+            numeric_id = 19,
+            title = 'Small [Flatdeck Barge]',
+            capacity_cargo_holds = 240,
+            buy_cost = 28,
+            fixed_run_cost_factor = 3.5,
+            fuel_run_cost_factor = 1.0,
+            offsets = [[-15, -38], [-79, -21], [-66, -25], [-38, -22], [-14, -36], [-78, -22], [-68, -25], [-38, -20]],
+            buy_menu_width = 96,
+            intro_date = 1870,
+            buy_menu_bb_xy = [620, 28],
+            effects = ['EFFECT_SPRITE_STEAM, 8, 0, 24'])
+
+ship.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
