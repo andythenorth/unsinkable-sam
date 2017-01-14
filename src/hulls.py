63910ad95ef29495aafd7db6f5cdabdccfd44576
andythenorth <mail@andythenorth.co.uk>
2017-01-14 12:21:15 +0000

    Change: support for wake sprites per hull

--- src/hulls.py
+++ src/hulls.py
@@ -6,7 +6,6 @@ class Hull(object):
     # there's only instance of each, so I just used class properties...boilerplate reduced to minimum eh :)
     size_class = 'Define in subclass'
     # length
-    wake_spritesheet_name = 'ship_128px'
     # hull spritesheet file
     # vertical offset if needed, accounting for buy menu especially
     # mask_name
@@ -18,42 +17,49 @@ class LargeShipHouseRear(Hull):
     size_class = 'large'
     spritesheet_name = 'large_ship_house_rear'
     mask_name = 'ship_128px'
+    wake_name = 'ship_128px'
 
 
 class LargeShipHouseForward(Hull):
     size_class = 'large'
     spritesheet_name = 'test_large_house_rear'
     mask_name = 'ship_128px'
+    wake_name = 'ship_128px'
 
 
 class LargeRiverboatHouseRear(Hull):
     size_class = 'large'
     spritesheet_name = 'large_riverboat_house_rear'
     mask_name = 'ship_128px'
+    wake_name = 'ship_128px'
 
 
 class SmallRiverboatHouseRear(Hull):
     size_class = 'small'
     spritesheet_name = 'small_riverboat_house_rear'
     mask_name = 'ship_128px'
+    wake_name = 'ship_128px'
 
 
 class SmallShipHouseRear(Hull):
     size_class = 'small'
     spritesheet_name = 'small_ship_house_rear'
     mask_name = 'ship_64px'
+    wake_name = 'ship_64px'
 
 
 class TempMini(Hull):
     size_class = 'mini'
     spritesheet_name = 'mini_ship_house_rear'
     mask_name = 'ship_128px'
+    wake_name = 'ship_128px'
 
 
 class TempMicro(Hull):
     size_class = 'micro'
     spritesheet_name = 'micro_ship_house_rear'
     mask_name = 'ship_128px'
+    wake_name = 'ship_128px'
 
 
 registered_hulls = {'LargeRiverboatHouseRear': LargeRiverboatHouseRear(),
