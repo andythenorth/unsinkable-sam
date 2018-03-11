733a544cdb6a6a043822a92ce2e45de739083a32
andythenorth <mail@andythenorth.co.uk>
2018-03-11 08:51:22 +0000

    Change: WIP on small ships, small trawler, micro trawler

---
+++ src/ships/edibles_tanker_small.py
@@ -0,0 +1,16 @@
+import global_constants
+from ship import EdiblesTanker
+
+ship = EdiblesTanker(id = 'edibles_tanker_small',
+                    numeric_id = 32,
+                    title = '[Edibles Tanker]',
+                    hull = 'ShipHouseRearSmall',
+                    buy_cost = 64,
+                    fixed_run_cost_factor = 12.0,
+                    fuel_run_cost_factor = 1.1,
+                    intro_date = 1870,
+                    effect_type = 'EFFECT_SPRITE_DIESEL')
+
+ship.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
