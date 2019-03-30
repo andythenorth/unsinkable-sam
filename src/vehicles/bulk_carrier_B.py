3b3a927977ec4dde3dbaee7a752ede341899d17a
andythenorth <mail@andythenorth.co.uk>
2019-03-30 08:36:23 +0000

    Change: add Mini sizes for Bulk Carrier, Flatdeck Barge, Tanker

---
+++ src/vehicles/bulk_carrier_B.py
@@ -0,0 +1,15 @@
+from ship import BulkCarrier
+
+ship = BulkCarrier(id='bulk_carrier_B',
+                   numeric_id=35,
+                   name='[Bulk Barge]',
+                   subtype='B',
+                   hull='BargeHouseRear',
+                   buy_cost=28,
+                   fixed_run_cost_factor=3.5,
+                   fuel_run_cost_factor=1.0,
+                   intro_date=1870,
+                   effect_type='EFFECT_SPRITE_DIESEL',
+                   sprites_complete=False)
+
+
---
+++ src/vehicles/flatdeck_barge_B.py
@@ -0,0 +1,15 @@
+from ship import FlatDeckBarge
+
+ship = FlatDeckBarge(id='flatdeck_barge_B',
+                     numeric_id=36,
+                     name='[Flatdeck Barge]',
+                     subtype='B',
+                     hull='BargeHouseRear',
+                     buy_cost=28,
+                     fixed_run_cost_factor=3.5,
+                     fuel_run_cost_factor=1.0,
+                     intro_date=1870,
+                     effect_type='EFFECT_SPRITE_DIESEL',
+                     cargo_length=7)
+
+
