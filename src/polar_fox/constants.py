720b7965b4661dcf2d6aebd7bd19832ef597e022
andythenorth <mail@andythenorth.co.uk>
2019-04-04 07:03:01 +0100

    Change: updated Polar Fox adding Coal Tarr colour remap for tankers

--- src/polar_fox/constants.py
+++ src/polar_fox/constants.py
@@ -198,6 +198,8 @@ piece_sprites_to_cargo_labels_maps = {'barrels_silver': ['BEER', 'DYES', 'EOIL',
 # DFLT label is a hack to support cargos with no specific sprites (including unknown cargos), and should not be added to cargo translation table
 tanker_livery_recolour_maps = (("OIL_", {136: 1, 137: 2, 138: 3, 139: 4,
                                          140: 5, 141: 6, 142: 7, 143: 8}),
+                                ("CTA", {136: 1, 137: 2, 138: 3, 139: 4,
+                                         140: 5, 141: 6, 142: 7, 143: 8}),
                                # see note on DFLT above
                                ("DFLT", {136: 198, 137: 199, 138: 200, 139: 201,
                                          140: 202, 141: 203, 142: 204, 143: 205}),
