869686cd938625d91e364cfb45bf42d19267379d
Andy <andy@teamrubber.com>
2020-11-08 15:07:59 +0000

    Change: extend Reefer models

---
+++ src/vehicles/reefer_E.py
@@ -0,0 +1,11 @@
+from ship import Reefer
+
+ship = Reefer(id='reefer_E',
+              numeric_id=59,
+              name='Caribou [Reefer]',
+              subtype='E',
+              hull='ShipHouseForward',
+              fixed_run_cost_factor=3.5,
+              fuel_run_cost_factor=1.0,
+              intro_date=1870,
+              effect_type='EFFECT_SPRITE_STEAM')
