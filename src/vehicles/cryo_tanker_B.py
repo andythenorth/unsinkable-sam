b98ce30fbf59cf6469cefbec85c87a0ad54d8146
Andy <andy@teamrubber.com>
2020-11-08 19:20:24 +0000

    Change: extend Cryo tanker models

---
+++ src/vehicles/cryo_tanker_B.py
@@ -0,0 +1,13 @@
+from ship import CryoTanker
+
+ship = CryoTanker(id='cryo_tanker_B',
+                  numeric_id=63,
+                  name='Schmieder [Cryo Tanker]',
+                  subtype='D',
+                  hull='ShipHouseRear',
+                  fixed_run_cost_factor=2.0,
+                  fuel_run_cost_factor=1.8,
+                  intro_date=1870,
+                  str_type_info='SMALL_TANKER_COASTAL_INLAND',
+                  effect_type='EFFECT_SPRITE_DIESEL',
+                  sprites_complete=False)
