78801f663ae50a08b5722bca37c41de489dd74b7
Andy <andy@teamrubber.com>
2020-11-08 13:44:29 +0000

    Change: extend Edibles Tanker models

---
+++ src/vehicles/edibles_tanker_B.py
@@ -0,0 +1,11 @@
+from ship import EdiblesTanker
+
+ship = EdiblesTanker(id='edibles_tanker_B',
+                     numeric_id=58,
+                     name='Bounty [Edibles Tanker]',
+                     subtype='B',
+                     hull='BargeHouseRear',
+                     fixed_run_cost_factor=12.0,
+                     fuel_run_cost_factor=1.1,
+                     intro_date=1870,
+                     effect_type='EFFECT_SPRITE_DIESEL')
