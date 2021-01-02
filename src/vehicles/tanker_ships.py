a10e1a43a786ed4c42ec2d67d3095a48d56d1f71
Andy <andy@teamrubber.com>
2021-01-02 22:26:20 +0000

    Change: sprites complete for 3E Tanker, ship pixels complete for 2E tanker (needs hull)

--- src/vehicles/tanker_ships.py
+++ src/vehicles/tanker_ships.py
@@ -65,7 +65,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
 
