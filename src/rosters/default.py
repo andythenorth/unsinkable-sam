689b91432005e4ae93773e41899e2d44cae7d342
Andy <andy@teamrubber.com>
2021-01-03 08:52:01 +0000

    Change: brute-force a preview release by removing unfinished ships and levelling intro dates and speeds

--- src/rosters/default.py
+++ src/rosters/default.py
@@ -7,11 +7,10 @@ def main(disabled=False):
         numeric_id=1,
         # left as dict when copying from Iron Horse, assumes might split pax / freight / hovercraft etc in future?  Bets that I end up making it just a list?
         intro_dates={
-            #"DEFAULT": [1860, 1900, 1960, 2020],
-            "DEFAULT": [1860, 1860, 1860, 1860],
+            "DEFAULT": [1900, 1900, 1900, 1900],
         },
         speeds={
-            "freight": {0: 25, 1950: 35},
+            "freight": {0: 25, 1950: 25},
             "fast_freight": {},  # unused currently
             "pax_mail": {0: 35, 1950: 45, 1980: 70},
         },
