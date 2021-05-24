f3daffc130fd6f1e57df551c2e1c887620f5c9ad
Andy <andy@teamrubber.com>
2021-05-24 21:50:37 +0100

    Change: add recently completed ships

--- src/vehicles/bulk_ships.py
+++ src/vehicles/bulk_ships.py
@@ -22,7 +22,6 @@ def main():
         effect_type="EFFECT_SPRITE_DIESEL",
         sprites_complete=True,
     )
-    """
     ship = BulkShip(
         numeric_id=54,
         name="Saltlick",
@@ -42,7 +41,7 @@ def main():
         effect_type="EFFECT_SPRITE_DIESEL",
         sprites_complete=True,
     )
-    """
+
     ship = BulkShip(
         numeric_id=51,
         name="Gravelly",
