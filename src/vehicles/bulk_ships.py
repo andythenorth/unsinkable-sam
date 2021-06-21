9ce090f563e99bbbccfd54c2d5cabba79030e3a0
Andy <andy@teamrubber.com>
2021-06-21 07:56:49 +0100

    Change: WIP on Alligator mini bulker sprites

--- src/vehicles/bulk_ships.py
+++ src/vehicles/bulk_ships.py
@@ -69,5 +69,5 @@ def main():
         subtype="F",
         hull="ShipHouseRear",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=False, # needed wake sprites and smoke positions sorting - done yet?
     )
