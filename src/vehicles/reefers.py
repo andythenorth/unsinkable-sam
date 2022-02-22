bc5a26dc435c13a8acaff94f781cb6aba9f7f978
Andy <andy@teamrubber.com>
2022-02-22 08:57:38 +0000

    Change: enabled gen 3D reefer in preview

--- src/vehicles/reefers.py
+++ src/vehicles/reefers.py
@@ -24,7 +24,7 @@ def main():
         cargo_length=6,
         sprites_complete=False,
     )
-
+    """
     ship = Reefer(
         numeric_id=14,
         name="Kodiak",
@@ -34,7 +34,7 @@ def main():
         effect_type="EFFECT_SPRITE_DIESEL",
         sprites_complete=True,
     )
-
+    """
     ship = Reefer(
         numeric_id=59,
         name="Caribou",
