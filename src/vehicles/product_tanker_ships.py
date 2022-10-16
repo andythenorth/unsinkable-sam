52eb4ab874916b47776c729e590002ceb1701663
Andy <andy@teamrubber.com>
2022-10-16 09:46:40 +0100

    Change: sprites complete for gen 3D product tanker

--- src/vehicles/product_tanker_ships.py
+++ src/vehicles/product_tanker_ships.py
@@ -43,7 +43,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     ship = ProductTankerShip(
