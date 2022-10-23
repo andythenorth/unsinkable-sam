76a9b9d10d0d3609122800badc1feb2c7f0b1338
Andy <andy@teamrubber.com>
2022-10-23 14:40:53 +0100

    Change: sprites complete for Cryo Tankers

--- src/vehicles/cryo_tankers.py
+++ src/vehicles/cryo_tankers.py
@@ -10,7 +10,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     ship = CryoTanker(
@@ -21,7 +21,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     ship = CryoTanker(
@@ -32,7 +32,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     ship = CryoTanker(
@@ -43,7 +43,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     ship = CryoTanker(
@@ -54,7 +54,7 @@ def main():
         hull="ShipHouseRear",
         str_type_info="SMALL_TANKER_COASTAL_INLAND",
         effect_type="EFFECT_SPRITE_DIESEL",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     ship = CryoTanker(
@@ -65,5 +65,5 @@ def main():
         hull="ShipHouseRear",
         str_type_info="COASTAL_TANKER",
         effect_type="EFFECT_SPRITE_AIRCRAFT_BREAKDOWN_SMOKE",
-        sprites_complete=False,
+        sprites_complete=True,
     )
